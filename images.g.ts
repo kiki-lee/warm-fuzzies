c4891a51d9cdbd313d340c32367a8f1f64933ac6
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2023-01-30 18:39:15 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,134 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "p1":return img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 2 2 2 2 f f f 2 2 2 2 f f 
+. f 2 2 2 2 2 2 f 2 2 2 2 2 2 f 
+. f 2 2 2 2 2 2 2 2 1 1 1 2 2 f 
+. f 2 2 2 2 2 2 2 2 1 1 1 2 2 f 
+. f 2 2 2 2 2 b b b 1 1 1 2 2 f 
+. f 2 2 2 2 b b b b b 2 2 2 2 f 
+. f f 2 2 b b b b b b b 2 2 f f 
+. . f f 2 b b b b b b b 2 f f . 
+. . . f f b b b b b b b f f . . 
+. . . . f f b b b b b f f . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "p2":return img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 8 8 8 8 f f f 8 8 8 8 f f 
+. f 8 8 8 8 8 8 f 8 8 8 8 8 8 f 
+. f 8 8 8 8 8 8 8 8 1 1 1 8 8 f 
+. f 8 8 8 8 8 8 8 8 1 1 1 8 8 f 
+. f 8 8 8 8 8 9 9 9 1 1 1 8 8 f 
+. f 8 8 8 8 9 9 9 9 9 8 8 8 8 f 
+. f f 8 8 9 9 9 9 9 9 9 8 8 f f 
+. . f f 8 9 9 9 9 9 9 9 8 f f . 
+. . . f f 9 9 9 9 9 9 9 f f . . 
+. . . . f f 9 9 9 9 9 f f . . . 
+. . . . . f f 9 9 9 f f . . . . 
+. . . . . . f f 9 f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "p3":return img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 4 4 4 4 f f f 4 4 4 4 f f 
+. f 4 4 4 4 4 4 f 4 4 4 4 4 4 f 
+. f 4 4 4 4 4 4 4 4 1 1 1 4 4 f 
+. f 4 4 4 4 4 4 4 4 1 1 1 4 4 f 
+. f 4 4 4 4 4 5 5 5 1 1 1 4 4 f 
+. f 4 4 4 4 5 5 5 5 5 4 4 4 4 f 
+. f f 4 4 5 5 5 5 5 5 5 4 4 f f 
+. . f f 4 5 5 5 5 5 5 5 4 f f . 
+. . . f f 5 5 5 5 5 5 5 f f . . 
+. . . . f f 5 5 5 5 5 f f . . . 
+. . . . . f f 5 5 5 f f . . . . 
+. . . . . . f f 5 f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "p4":return img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 7 7 7 7 f f f 7 7 7 7 f f 
+. f 7 7 7 7 7 7 f 7 7 7 7 7 7 f 
+. f 7 7 7 7 7 7 7 7 1 1 1 7 7 f 
+. f 7 7 7 7 7 7 7 7 1 1 1 7 7 f 
+. f 7 7 7 7 7 6 6 6 1 1 1 7 7 f 
+. f 7 7 7 7 6 6 6 6 6 7 7 7 7 f 
+. f f 7 7 6 6 6 6 6 6 6 7 7 f f 
+. . f f 7 6 6 6 6 6 6 6 7 f f . 
+. . . f f 6 6 6 6 6 6 6 f f . . 
+. . . . f f 6 6 6 6 6 f f . . . 
+. . . . . f f 6 6 6 f f . . . . 
+. . . . . . f f 6 f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "hug jar":return img`
+.1111ddddddddddddddddddddd111.
+dddddddddddddddddddddddddddddd
+bbbbbbdddddddddddddddddddbbbbb
+dddddddddddddddddddddddddddddd
+.dddddddddddddddddddddddddddd.
+..dddddddddddddddddddddddddd..
+..dddddddddddddddddddddddddd..
+...dddddddddddddddddddddddd...
+...dddddddddddddddddddddddd...
+...dddddddddddddddddddddddd...
+....dddddddddddddddddddddd....
+....dddddddddddddddddddddd....
+....dddddddddddddddddddddd....
+....dddddddddddddddddddddd....
+...dddddddddddddddddddddddd...
+..ddddddddddddddddddddddd1dd..
+.ddddddddddddddddddddddddd1dd.
+ddddddddddddddddddddddddddd1dd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddd
+.dddddddddddddddddddddddddddd.
+.ddbbdddddddddddddddddddddddd.
+..dddbdddddddddddddddddddddd..
+....dddddddddddddddddddddd....
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
